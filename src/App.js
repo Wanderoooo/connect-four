fbd1dac23d5ae2f1d39c2944bc285d12d1b1d746
Alissa Guo <137996848+Wanderoooo@users.noreply.github.com>
2023-08-07 21:55:05 -0700

    added both winning options

--- src/App.js
+++ src/App.js
@@ -2,21 +2,74 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  let firstIndexSameIds = 0;
-  let secondIndexSameIds = 0;
 
   function handleWin({selectedIdArray, idsDoubleArray}) {
-    const isWin = 
-    checkGreaterEqualsFour(selectedIdArray, idsDoubleArray)
-    // !!! do cond 2
-    
+    const isWin = checkGreaterEqualsFour(selectedIdArray, idsDoubleArray) ||
+    checkCross(selectedIdArray, idsDoubleArray);
 
     return isWin;
   }
 
+  function checkCross(selectedIdArray, idsDoubleArray) {
+    const diagSolutionsTArray = makeDiagSolutionsTArray(selectedIdArray)
+    const includeSelectedIdDArray = idsDoubleArray.push(selectedIdArray)
+
+    for (let i = 0; i < diagSolutionsTArray.length; i++) {
+      if (diagSolutionsTArray.every(element => includeSelectedIdDArray.includes(element))) {
+        return true;
+      }
+    }
+
+  }
+
+  function makeDiagSolutionsDArray(selectedIdArray) {
+    let allDiagSolnTArray = [
+      [
+        [selectedIdArray[0] - 3, selectedIdArray[0] + 3],
+        [selectedIdArray[0] - 2, selectedIdArray[0] + 2],
+        [selectedIdArray[0] - 1, selectedIdArray[0] + 1],
+        [selectedIdArray[0], selectedIdArray[0]]
+      ]
+
+      [
+        [selectedIdArray[0] - 2, selectedIdArray[0] + 2],
+        [selectedIdArray[0] - 1, selectedIdArray[0] + 1],
+        [selectedIdArray[0], selectedIdArray[0]],
+        [selectedIdArray[0] + 1, selectedIdArray[0] - 1]
+      ]
+    ]
+
+    for (let i = 0; i < 8; i++) {
+      let aSoln = [];
+      let bSoln = [];
+
+      for (let j = 3; j >= 0; j--) {
+        aSoln = [
+          [selectedIdArray[0] - j, selectedIdArray[0] + j],
+          [selectedIdArray[0] + 1 - j, selectedIdArray[0] - 1 + j],
+          [selectedIdArray[0] + 2 - j, selectedIdArray[0] - 2 + j],
+          [selectedIdArray[0] + 3 - j, selectedIdArray[0] - 3 + j]
+        ]
+
+        bSoln = [
+          [selectedIdArray[0] - j, selectedIdArray[0] - j],
+          [selectedIdArray[0] + 1 - j, selectedIdArray[0] + 1 - j],
+          [selectedIdArray[0] + 2 - j, selectedIdArray[0] + 2 - j],
+          [selectedIdArray[0] + 3 - j, selectedIdArray[0] + 3 - j]
+        ]
+      }
+
+      allDiagSolnTArray.push(aSoln)
+      allDiagSolnTArray.push(bSoln)
+    }
+
+
+    return allDiagSolnTArray;
+  }
+
   function checkGreaterEqualsFour(selectedIdArray, idsDoubleArray) {
     let firstIndexNumOfOccur = 0;
-    let secondIndexNumOfOccur =0;
+    let secondIndexNumOfOccur = 0;
 
     for (let i = 0; i < idsDoubleArray.length; i++) {
       if (idsDoubleArray[i][0] === selectedIdArray[0]) {
