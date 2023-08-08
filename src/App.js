1354fc202f4a3882e289cab47b3b4b9b99b1a866
Alissa Guo <137996848+Wanderoooo@users.noreply.github.com>
2023-08-07 20:14:34 -0700

    set up condition 1 of winning

--- src/App.js
+++ src/App.js
@@ -2,6 +2,34 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let firstIndexSameIds = 0;
+  let secondIndexSameIds = 0;
+
+  function handleWin({selectedIdArray, idsDoubleArray}) {
+    const isWin = 
+    checkGreaterEqualsFour(selectedIdArray, idsDoubleArray)
+    // !!! do cond 2
+    
+
+    return isWin;
+  }
+
+  function checkGreaterEqualsFour(selectedIdArray, idsDoubleArray) {
+    let firstIndexNumOfOccur = 0;
+    let secondIndexNumOfOccur =0;
+
+    for (let i = 0; i < idsDoubleArray.length; i++) {
+      if (idsDoubleArray[i][0] === selectedIdArray[0]) {
+        firstIndexNumOfOccur++;
+      } else if (idsDoubleArray[i][1] === selectedIdArray[1]) {
+        secondIndexNumOfOccur++;
+      }
+  }
+
+  return Math.max(firstIndexNumOfOccur, secondIndexNumOfOccur) >= 4;
+}
+
+
   return (
     <div className="App">
       <header className="App-header">
